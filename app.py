d0b878043d9cdd202196de392cd529dd8273dadc
Oyashi <oyeshin@hotmail.com>
2020-11-26 11:41:59 -0500

    Fixed local run + ignore *.db

--- app.py
+++ app.py
@@ -8,11 +8,12 @@ from resources.user import UserRegister
 from resources.items import Item, ItemList
 from resources.store import Store, StoreList
 
+from db import db
 
 app = Flask(__name__)
 # If on heroku or environ variable defined, then connect to DATABASE_URL
 # else connect to sqlite:///datab.db
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///datab.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.secret_key = 'oyesh'
 api = Api(app)
@@ -27,8 +28,6 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-    from db import db
-
     db.init_app(app)
 
     if app.config['DEBUG']:
